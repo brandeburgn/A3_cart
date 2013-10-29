e681116dd21569b06e3facdc1af0c634e28200b7
Nathan Brandeburg <brandeburgn@Nathans-MacBook-Pro.local>
2013-10-29 13:46:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+A3Cart::Application.config.secret_key_base = '92cfff3f1f9c8f73312739a40abfa21a873d1972e22d8acec2ecc8410e1d510e41f9fd4350842af80cd6250bd3ee3004d78a5906735c47f181dee13e22f43c0d'
